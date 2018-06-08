646016eea97f76eb89ea9b6e9bea964481531641
valentin.kozlov <valentin.kozlov@gmail.com>
2018-06-08 11:15:25 +0200

    change ppn:1 to ppn:48, i.e. number of processes per node

--- sub_njobs_visu.sh
+++ sub_njobs_visu.sh
@@ -4,6 +4,6 @@ NJOB=1
 for j in $(seq 1 $NJOB)
 do 
     echo "Submitting job $j out of $NJOB.."
-    msub -N udocker -l nodes=1:ppn=1:visu,walltime=3:00:00 $JOB2RUN
+    msub -N udocker -l nodes=1:ppn=48:visu,walltime=3:00:00 $JOB2RUN
     sleep 1s
 done
