1e98d24e7acf54e1378bc1497bf3ae1e563c4523
valentin.kozlov <valentin.kozlov@kit.edu>
2018-06-20 20:19:07 +0200

    update udocker_pull.sh

--- udocker_pull.sh
+++ udocker_pull.sh
@@ -16,17 +16,24 @@ UDOCKER_DIR="$PROJECT/.udocker"  # udocker main directory.
 
 # Default settings. Will be overwritten from the command line (preferred way!)
 DEFAULTIMG="tensorflow/tensorflow:1.6.0-gpu"
-#DEFAULTIMG="mxnet/python:1.2.0_gpu_cuda9"
+UCONTAINER="tf160gpu"
+###################
+
+USAGEMESSAGE="Either run as \n
+              $ $0 DOCKERIMG UCONTAINER (example: $0 tensorflow/tensorflow:1.7.0-gpu tf170gpu) \n
+              or just \n
+              $ $0 (default is set in $0, e.g. DEFAULTIMG=tensorflow/tensorflow:1.6.0-gpu and UCONTAINER=tf160gpu)"
+
 if [ $# -eq 0 ]; then
     DOCKERIMG=$DEFAULTIMG
-elif [ $# -eq 1 ]; then
+elif [ $# -eq 2 ]; then
     DOCKERIMG=$1
+    UCONTAINER=$2
 else
     echo "#############################################################"
-    echo "#  ERROR! Wrong execution. Either run as"
-    echo "#  $> $0 DOCKERIMG (example: $0 tensorflow/tensorflow:1.7.0-gpu)"
-    echo "#  or just"
-    echo "#  $> $0 (default is set in $0, e.g. DEFAULTIMG=tensorflow/tensorflow:1.6.0-gpu)"
+    echo  "ERROR! Wrong execution. "
+    shopt -s xpg_echo
+    echo " "$USAGEMESSAGE
     echo "#############################################################"
     exit 1
 fi
@@ -35,13 +42,6 @@ fi
 DOCKERTAG=${DOCKERIMG#*:}
 
 ##########################
-
-[[ $DOCKERIMG = *"mxnet"* ]]  && UPREFFIX="mx"
-[[ $DOCKERIMG = *"tensorflow"* ]]  && UPREFFIX="tf"
-[[ $DOCKERIMG = *"python"* ]]  && UPREFFIX=$UPREFFIX"py"
-
-UCONTAINER="$UPREFFIX$DOCKERTAG"
-UCONTAINER="${UCONTAINER//./}"
 
 IFExist=$(udocker ps |grep "'$UCONTAINER'")
 if [ ${#IFExist} -le 1 ]; then
@@ -64,7 +64,8 @@ if [ ${#IFExist} -le 1 ]; then
     fi
 else
     echo "###########################################"
-    echo "  contrainer $UCONTAINER already exists!  "
-    echo "  note the name and use it for jobs       "
+    echo "  contrainer $UCONTAINER already exists!   "
+    echo "  note the name and use it for jobs        "
+    echo "  or delete it with $ udocker rm $UCONTAINER "
     echo "###########################################"
 fi
